6d7b7e9fecc1da74c7b3f1e07998f93c661148c6
yangweifeng <yangweifeng@joyy.com>
2023-02-22 16:13:48 +0800

    项目初始化

---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <h1>这是首页</h1>
+  <h1>{{ x }}, {{ y }}</h1>
+  <TodoList />
+  <Counter />
+</template>
+<script setup>
+import { ref } from 'vue'
+import TodoList from '../components/Todolist.vue'
+import Counter from '../components/Counter.vue'
+import { useMouse } from '../utils/mouse'
+let x = ref(0)
+let y = ref(0)
+let obj = useMouse()
+x.value = obj.x
+y.value = obj.y
+</script>
